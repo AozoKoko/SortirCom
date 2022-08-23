09c7304977c87e7bc271dfedcd536fcdd506a4c6
Pierrick <pierrick2611@gmail.com>
2022-08-23 16:29:20 +0200

    Fix Login [WIP]

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->getConnection()->connect();
-        $connected = $em->getConnection()->isConnected();
-        if($connected)
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+            return $this->render('home/login.html.twig',['last_username' => $lastUsername, 'error' => $error]);
+
+    }
+
+    /**
+     * @Route("/main", name="app_main")
+     */
+    public function main(): Response
+    {
             return $this->render('home/main.html.twig');
-        else
-            return $this->json('ok');
     }
 
 }
