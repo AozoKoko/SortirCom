d1a5a7f7627d7c25dd6a1b6e45b7b2bb7b89c956
Xavier Houchard <xavier.houchard2022@campus-eni.fr>
2022-08-26 09:59:59 +0200

    création page "Affichage d'un utilisateur"

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,6 +8,7 @@ use App\Form\ParticipantType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -36,7 +37,7 @@ class ProfileController extends AbstractController
     /**
      * @Route("/edit-profile/", name="app_profile_edit")
      */
-    public function editProfile(Request $request): Response
+    public function editProfile(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -56,10 +57,21 @@ class ProfileController extends AbstractController
         $participantForm->handleRequest($request);
 
         if ($participantForm->isSubmitted() && $participantForm->isValid()) {
-            //dump($participantForm);
+
             $participantToSave = $participantForm->getData();
+            $user = $participantForm->get('user')->getData();
+            $password = $user->getPassword();
+            dump($password);
+            $currentUser->setPassword(
+                $passwordHasher->hashPassword(
+                    $currentUser,
+                    $password
+                )
+            );
+
 
             $em->persist($participantToSave);
+            $em->persist($currentUser);
             $em->flush();
 
             return $this->redirectToRoute('app_vue_profile', [
@@ -70,5 +82,25 @@ class ProfileController extends AbstractController
         return $this->render('profile/profileEdit.html.twig', [
             "participantForm" => $participantForm->createView()
         ]);
+    }
+
+    /**
+     * @Route("/vue-other-profile/{id}", name="app_vue_other_profile")
+     */
+    public function displayOtherUserProfile($id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $userRepo = $em->getRepository(User::class);
+        //on récupère le USER grâce à son id rentré en paramètre.
+        $chosenUser = $userRepo->find($id);
+
+
+        $participantRepo = $em->getRepository(Participant::class);
+        //on récupère le participant grâce à son id retourné par l'USER.
+        $chosenParticipant = $participantRepo->find(['id' => $chosenUser->getId()]);
+
+
+        return $this->render('profile/otherUserProfile.html.twig', ["participant" => $chosenParticipant]);
     }
 }
